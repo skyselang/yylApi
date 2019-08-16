a1c8ec534be907e4f3c7efdb12b0255dc253a855
skyselang <215817969@qq.com>
2019-08-16 14:28:57 +0800

    显示更新时间

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\index\controller;
 
 use think\Controller;
@@ -7,26 +8,27 @@ use think\facade\Config;
 use think\facade\Session;
 
 class Index extends Base
-{	
+{
     public function index()
     {
         $user_id = Session::get('user_id');
 
         $project = Db::name('relation')
             ->alias('r')
-            ->join('project p','p.project_id=r.project_id')
-            ->join('interface i','i.project_id=p.project_id')
-            ->where(['r.user_id'=>$user_id,'p.is_delete'=>0,'p.is_disable'=>0,'i.is_delete'=>0,'i.is_disable'=>0])
-            ->field('r.id,r.user_id,p.project_id,p.project_name,p.project_desc,p.create_time,count(i.interface_id) as count,max(i.update_time) as last_time')
+            ->join('project p', 'p.project_id=r.project_id')
+            ->join('interface i', 'i.project_id=p.project_id')
+            ->field('r.id, r.user_id, p.project_id, p.project_name, p.project_desc, p.create_time,count(i.interface_id) as count,max(i.update_time) as last_time')
+            ->where(['r.user_id' => $user_id, 'p.is_delete' => 0, 'p.is_disable' => 0, 'i.is_delete' => 0, 'i.is_disable' => 0])
             ->order('p.project_id desc')
             ->group('i.project_id')
             ->select();
-            
-        foreach ($project as $k => $v) {
-            $project[$k]['last_time_ago'] = format_date(strtotime($v['last_time']));
+        if ($project) {
+            foreach ($project as $k => $v) {
+                $project[$k]['last_time_ago'] = format_date(strtotime($v['last_time']));
+            }
         }
 
-        $this->assign('project',$project);
+        $this->assign('project', $project);
 
         return $this->fetch();
     }
